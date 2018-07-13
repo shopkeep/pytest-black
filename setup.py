86bf6d2d79f8185a08111abbad32ffb2c951874c
Patrick Carey <paddy@shopkeep.com>
2018-07-13 10:59:12 +0100

    Bump version to 0.1.7

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-black",
-    version="0.1.6",
+    version="0.1.7",
     author="ShopKeep Inc",
     author_email="oss@shopkeep.com",
     maintainer="ShopKeep Inc",
