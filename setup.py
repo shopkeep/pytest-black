cd6d563a0f7cd9b0339e8fa395747535f4415ee2
Declan Traynor <declan@shopkeep.com>
2018-08-07 17:15:35 +0100

    Bump version to 0.1.9

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name="pytest-black",
-    version="0.1.8",
+    version="0.1.9",
     author="ShopKeep Inc",
     author_email="oss@shopkeep.com",
     maintainer="ShopKeep Inc",
