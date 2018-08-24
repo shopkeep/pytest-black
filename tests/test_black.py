52d0c07e9ab9ac8848e2ea725a31856b9b1fe2d8
Simon King <simon@simonking.org.uk>
2018-08-24 11:44:57 +0100

    Skip repeated tests if the file's mtime hasn't changed
    The mtimes are stored in pytest's cache. The implementation is based
    on the equivalent feature in pytest-flake8.

--- tests/test_black.py
+++ tests/test_black.py
@@ -33,3 +33,31 @@ def test_pass(testdir):
 
     result = testdir.runpytest("--black")
     result.assert_outcomes(passed=1)
+
+
+def test_mtime_cache(testdir):
+    """Assert test is skipped when file hasn't changed
+    """
+    p = testdir.makepyfile(
+        """
+        def hello():
+            print("Hello, world!")
+    """
+    )
+    # replace trailing newline (stripped by testdir.makepyfile)
+    contents = p.read() + "\n"
+    p.write(contents)
+
+    # Test once to populate the cache
+    result = testdir.runpytest("--black")
+    result.assert_outcomes(passed=1)
+
+    # Run it again, it should be skipped
+    result = testdir.runpytest("--black", "-rs")
+    result.assert_outcomes(skipped=1)
+    result.stdout.fnmatch_lines(["SKIP * previously passed black format checks"])
+
+    # Update the file and test again.
+    p.write(contents)
+    result = testdir.runpytest("--black")
+    result.assert_outcomes(passed=1)
